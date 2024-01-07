c0cf4c14210490a4e0032a39fe59c0dfff7e7c38
Ilya Finkelstein <ilya@MacBook-Pro.local>
2024-01-06 20:42:58 -0600

    add substitute.el package

--- library/setup/my-setup-search.el
+++ library/setup/my-setup-search.el
@@ -67,6 +67,23 @@
 (use-package visual-regexp-steroids
   :commands (vr/select-query-replace))
 
+;;;; substitute -- rapidly change search/replace in buffer
+(use-package  substitute
+  :commands (substitute-target-in-buffer)
+  :config
+  ;; Set this to nil if you do not like visual feedback on the matching
+  ;; target.  Default is t.
+  (setq substitute-highlight t)
+
+  ;; Set this to t if you want to always treat the letter casing
+  ;; literally.  Otherwise each command accepts a `C-u' prefix
+  ;; argument to do this on-demand.
+  (setq substitute-fixed-letter-case t)
+
+  ;; If you want a message reporting the matches that changed in the
+  ;; given context.  We don't do it by default.
+  (add-hook 'substitute-post-replace-functions #'substitute-report-operation))
+
 ;;; Provide
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'my-setup-search)
