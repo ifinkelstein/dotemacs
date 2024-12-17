0ab778418e55ffd78df09d7e98f31e8fc2c364ec
Ilya Finkelstein <ilya@wireless-10-145-58-163.public.utexas.edu>
2024-12-17 14:30:03 -0600

    pin meow to melpa-stable; some cleanup

--- library/setup/my-setup-keybindings.el
+++ library/setup/my-setup-keybindings.el
@@ -238,6 +238,7 @@
 ;;;; Meow
 ;; Note load Meow before loading personal keybindings, otherwise some might get clobbered
 (use-package meow
+  :pin melpa-stable
   :config
   ;; set colors in bespoke theme
   (setq meow-use-dynamic-face-color nil)
@@ -726,37 +727,7 @@
               ("t" org-table-transpose-table-at-point "Org mode table")
               ("q" nil "cancel" :color blue))))
 
-;;;;; Hydra Rectangle
-(with-eval-after-load 'hydra
-  (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
-                             :color pink
-                             :hint nil
-                             :post (deactivate-mark))
-    "
-  ^_k_^       _w_ copy      _o_pen       _N_umber-lines            |\\     -,,,--,,_
-_h_   _l_     _y_ank        _t_ype       _e_xchange-point          /,`.-'`'   ..  \-;;,_
-  ^_j_^       _d_ kill      _c_lear      _r_eset-region-mark      |,4-  ) )_   .;.(  `'-'
-^^^^          _u_ndo        _g_ quit     ^ ^                     '---''(./..)-'(_\_)
-"
-    ("k" rectangle-previous-line)
-    ("j" rectangle-next-line)
-    ("h" rectangle-backward-char)
-    ("l" rectangle-forward-char)
-    ("d" kill-rectangle)                    ;; C-x r k
-    ("y" yank-rectangle)                    ;; C-x r y
-    ("w" copy-rectangle-as-kill)            ;; C-x r M-w
-    ("o" open-rectangle)                    ;; C-x r o
-    ("t" string-rectangle)                  ;; C-x r t
-    ("c" clear-rectangle)                   ;; C-x r c
-    ("e" rectangle-exchange-point-and-mark) ;; C-x C-x
-    ("N" rectangle-number-lines)            ;; C-x r N
-    ("r" (if (region-active-p)
-             (deactivate-mark)
-           (rectangle-mark-mode 1)))
-    ("u" undo nil)
-    ("g" nil)))
 
-;; TODO: Add org and markdown keybindings
 ;;; End keybindings
 (provide 'my-setup-keybindings)
 
