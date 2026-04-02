37cccb69182870c52b721d5e72fb429401adbb4b
ilya <ilya@finkelsteinlab.org>
2026-04-01 20:57:13 -0500

    bookmarks: expand javelin to 10 slots, cleanup and simplify
    - Add all 10 javelin slots (1-5 and 6-0 in two columns)
    - Use DWIM javelin-go-or-assign-to-N functions (pin if empty, jump if set)
    - Consolidate all declare-function/defvar at top, grouped by package
    - Remove duplicate declarations
    - Unify three embark pin functions into one my-embark-javelin-pin
    - Simplify my-bookmarks--javelin-slot-desc with if-let*
    - Remove unused declarations (javelin-go-to, javelin--bookmark-display-target)
    - Reuse my-embark-javelin-pin in dogears-to-javelin
    - Better code organization with section headers

--- library/setup/my-setup-bookmarks.el
+++ library/setup/my-setup-bookmarks.el
@@ -8,54 +8,62 @@
 
 (require 'transient)
 
-;;* Emacs Bookmarks (built-in)
-(use-package bookmark
-  :ensure nil
-  :defer 2
-  :config
-  (setq bookmark-default-file (concat my-cache-dir "bookmarks")))
+;;; External declarations
 
-;;* Casual (transient UI for bookmark list buffer)
-(use-package casual
-  :after bookmark
-  :bind (:map bookmark-bmenu-mode-map
-         ("M-o" . casual-bookmarks-tmenu)))
-
-;;* Dogears (automatic place history)
-(use-package dogears
-  :bind (:map global-map
-              ("M-g d" . dogears-go)
-              ("M-g M-b" . dogears-back)
-              ("M-g M-f" . dogears-forward)
-              ("M-g M-d" . dogears-list)
-              ("M-g M-r" . dogears-remember)
-              ("M-g M-D" . dogears-sidebar))
-  :config
-  (dogears-mode 1))
-
-;;* Javelin (harpoon-style pinned buffers)
-(use-package javelin
-  :bind (("M-g j" . javelin-add-file)
-         ("M-g /" . javelin-toggle-quick-menu)
-         ("M-g 1" . javelin-go-or-assign-to-1)
-         ("M-g 2" . javelin-go-or-assign-to-2)
-         ("M-g 3" . javelin-go-or-assign-to-3)
-         ("M-g 4" . javelin-go-or-assign-to-4)
-         ("M-g 5" . javelin-go-or-assign-to-5)))
-
-;;* Embark Integration
-;;
-;; Adds embark actions for javelin and dogears:
-;; - J on file/buffer/bookmark: pin to javelin slot
-;; - E on file: dogear this location
-;; - In dogears-list-mode: embark on entries with custom actions
-
-;; Declare external functions for byte-compiler
-(declare-function javelin-assign-to "javelin")
+;; Dogears
+(declare-function dogears-go "dogears")
+(declare-function dogears-back "dogears")
+(declare-function dogears-forward "dogears")
 (declare-function dogears-remember "dogears")
-
-;; Declare external variables for byte-compiler
+(declare-function dogears-list "dogears")
+(declare-function dogears-sidebar "dogears")
+(declare-function dogears-mode "dogears")
 (defvar dogears-list)
+(defvar dogears-idle)
+(defvar dogears-idle-timer)
+(defvar dogears-message)
+
+;; Javelin
+(declare-function javelin-add-file "javelin")
+(declare-function javelin-assign-to "javelin")
+(declare-function javelin-delete "javelin")
+(declare-function javelin-toggle-quick-menu "javelin")
+(declare-function javelin-go-to-next "javelin")
+(declare-function javelin-go-to-prev "javelin")
+(declare-function javelin-clear "javelin")
+(declare-function javelin--bookmark-positions "javelin")
+(declare-function javelin-go-or-assign-to-1 "javelin")
+(declare-function javelin-go-or-assign-to-2 "javelin")
+(declare-function javelin-go-or-assign-to-3 "javelin")
+(declare-function javelin-go-or-assign-to-4 "javelin")
+(declare-function javelin-go-or-assign-to-5 "javelin")
+(declare-function javelin-go-or-assign-to-6 "javelin")
+(declare-function javelin-go-or-assign-to-7 "javelin")
+(declare-function javelin-go-or-assign-to-8 "javelin")
+(declare-function javelin-go-or-assign-to-9 "javelin")
+(declare-function javelin-go-or-assign-to-10 "javelin")
+
+;; Bookmark
+(declare-function bookmark-get-bookmark "bookmark")
+(defvar bookmark-save-flag)
+(defvar bookmark-use-annotations)
+(defvar bookmark-fringe-mark)
+
+;; Consult
+(declare-function consult-bookmark "consult")
+(declare-function consult-register "consult")
+(declare-function consult-register-store "consult-register")
+(declare-function consult-mark "consult")
+(declare-function consult-global-mark "consult")
+(declare-function consult-recent-file "consult")
+
+;; Other
+(declare-function goto-last-change "goto-chg")
+(declare-function goto-last-change-reverse "goto-chg")
+(declare-function casual-editkit-registers-tmenu "casual-editkit-utils")
+(declare-function casual-editkit-bookmarks-tmenu "casual-editkit-utils")
+
+;; Embark
 (defvar embark-general-map)
 (defvar embark-file-map)
 (defvar embark-buffer-map)
@@ -63,31 +71,59 @@
 (defvar embark-target-finders)
 (defvar embark-keymap-alist)
 
-;;** Javelin pin actions for files and buffers
+;;; Package configuration
 
-(defun my-embark-javelin-pin-file (file)
-  "Pin FILE to a javelin slot."
-  (let ((slot (read-number "Javelin slot (1-9): " 1)))
-    (with-current-buffer (find-file-noselect file)
-      (javelin-assign-to slot)
-      (message "Pinned %s to javelin slot %d"
-               (file-name-nondirectory file) slot))))
+(use-package bookmark
+  :ensure nil
+  :defer 2
+  :config
+  (setq bookmark-default-file (concat my-cache-dir "bookmarks")))
 
-(defun my-embark-javelin-pin-buffer (buffer)
-  "Pin BUFFER to a javelin slot."
-  (let ((slot (read-number "Javelin slot (1-9): " 1)))
-    (with-current-buffer buffer
-      (javelin-assign-to slot)
-      (message "Pinned %s to javelin slot %d" buffer slot))))
+(use-package casual
+  :after bookmark
+  :bind (:map bookmark-bmenu-mode-map
+              ("M-o" . casual-bookmarks-tmenu)))
 
-(defun my-embark-javelin-pin-bookmark (bookmark)
-  "Promote BOOKMARK to a javelin slot."
-  (let ((slot (read-number "Javelin slot (1-9): " 1)))
-    (bookmark-jump bookmark)
-    (javelin-assign-to slot)
-    (message "Pinned bookmark to javelin slot %d" slot)))
+(use-package dogears
+  :bind (("M-g d" . dogears-go)
+         ("M-g M-b" . dogears-back)
+         ("M-g M-f" . dogears-forward)
+         ("M-g M-d" . dogears-list)
+         ("M-g M-r" . dogears-remember)
+         ("M-g M-D" . dogears-sidebar))
+  :config
+  (dogears-mode 1))
 
-;;** Dogears actions for files
+(use-package javelin
+  :bind (("M-g j" . javelin-add-file)
+         ("M-g /" . javelin-toggle-quick-menu)
+         ("M-g 1" . javelin-go-or-assign-to-1)
+         ("M-g 2" . javelin-go-or-assign-to-2)
+         ("M-g 3" . javelin-go-or-assign-to-3)
+         ("M-g 4" . javelin-go-or-assign-to-4)
+         ("M-g 5" . javelin-go-or-assign-to-5)
+         ("M-g 6" . javelin-go-or-assign-to-6)
+         ("M-g 7" . javelin-go-or-assign-to-7)
+         ("M-g 8" . javelin-go-or-assign-to-8)
+         ("M-g 9" . javelin-go-or-assign-to-9)
+         ("M-g 0" . javelin-go-or-assign-to-10)))
+
+;;; Embark integration
+
+(defun my-embark-javelin-pin (target)
+  "Pin TARGET (file, buffer, or bookmark) to a javelin slot."
+  (let ((slot (read-number "Javelin slot (1-10): " 1)))
+    (cond
+     ((bufferp target)
+      (with-current-buffer target
+        (javelin-assign-to slot)))
+     ((and (stringp target) (file-exists-p target))
+      (with-current-buffer (find-file-noselect target)
+        (javelin-assign-to slot)))
+     (t ; bookmark
+      (bookmark-jump target)
+      (javelin-assign-to slot)))
+    (message "Pinned to javelin slot %d" slot)))
 
 (defun my-embark-dogears-remember-file (file)
   "Remember FILE location in dogears."
@@ -95,8 +131,6 @@
     (find-file file)
     (dogears-remember)
     (message "Dogeared %s" (file-name-nondirectory file))))
-
-;;** Dogears target finder and keymap
 
 (defun my-embark-target-dogears-at-point ()
   "Target the dogear entry at point in `dogears-list-mode'."
@@ -117,20 +151,15 @@
 
 (defun my-embark-dogears-to-javelin (place)
   "Promote dogears PLACE to a javelin slot."
-  (let ((slot (read-number "Javelin slot (1-9): " 1)))
-    (bookmark-jump place)
-    (javelin-assign-to slot)
-    (message "Promoted to javelin slot %d" slot)))
+  (bookmark-jump place)
+  (my-embark-javelin-pin (current-buffer)))
 
 (defun my-embark-dogears-to-bookmark (place)
   "Save dogears PLACE as a named bookmark."
   (bookmark-jump place)
   (call-interactively #'bookmark-set))
 
-;;** Register with embark after it loads
-
 (with-eval-after-load 'embark
-  ;; Dogears keymap (must be defined after embark loads for parent)
   (defvar-keymap embark-dogears-map
     :doc "Keymap for embark actions on dogears entries."
     :parent embark-general-map
@@ -140,61 +169,29 @@
     "J"   #'my-embark-dogears-to-javelin
     "b"   #'my-embark-dogears-to-bookmark)
 
-  ;; Javelin actions on files, buffers, bookmarks
-  (keymap-set embark-file-map "J" #'my-embark-javelin-pin-file)
-  (keymap-set embark-buffer-map "J" #'my-embark-javelin-pin-buffer)
-  (keymap-set embark-bookmark-map "J" #'my-embark-javelin-pin-bookmark)
-
-  ;; Dogears action on files
+  (keymap-set embark-file-map "J" #'my-embark-javelin-pin)
+  (keymap-set embark-buffer-map "J" #'my-embark-javelin-pin)
+  (keymap-set embark-bookmark-map "J" #'my-embark-javelin-pin)
   (keymap-set embark-file-map "E" #'my-embark-dogears-remember-file)
 
-  ;; Dogears target finder and keymap
   (add-to-list 'embark-target-finders #'my-embark-target-dogears-at-point)
   (add-to-list 'embark-keymap-alist '(dogears . embark-dogears-map)))
 
-;;* Unified Bookmark Transient
+;;; Transient helpers
 
-(declare-function dogears-go "dogears")
-(declare-function dogears-back "dogears")
-(declare-function dogears-forward "dogears")
-(declare-function dogears-remember "dogears")
-(declare-function dogears-list "dogears")
-(declare-function dogears-sidebar "dogears")
-(declare-function dogears-mode "dogears")
-(declare-function bookmark-get-bookmark "bookmark")
-(declare-function javelin-add-file "javelin")
-(declare-function javelin-go-to "javelin")
-(declare-function javelin-go-to-1 "javelin")
-(declare-function javelin-go-to-2 "javelin")
-(declare-function javelin-go-to-3 "javelin")
-(declare-function javelin-go-to-4 "javelin")
-(declare-function javelin-go-to-5 "javelin")
-(declare-function javelin-delete "javelin")
-(declare-function javelin-toggle-quick-menu "javelin")
-(declare-function javelin-go-to-next "javelin")
-(declare-function javelin-go-to-prev "javelin")
-(declare-function javelin-clear "javelin")
-(declare-function javelin-assign-to "javelin")
-(declare-function javelin--bookmark-positions "javelin")
-(declare-function javelin--bookmark-display-target "javelin")
-(declare-function consult-bookmark "consult")
-(declare-function consult-register "consult")
-(declare-function consult-register-store "consult-register")
-(declare-function consult-mark "consult")
-(declare-function consult-global-mark "consult")
-(declare-function consult-recent-file "consult")
-(declare-function goto-last-change "goto-chg")
-(declare-function goto-last-change-reverse "goto-chg")
-(declare-function casual-editkit-registers-tmenu "casual-editkit-utils")
-(declare-function casual-editkit-bookmarks-tmenu "casual-editkit-utils")
+(defun my-bookmarks--javelin-slot-desc (n)
+  "Return description for javelin slot N."
+  (if-let* (((fboundp 'javelin--bookmark-positions))
+            (positions (javelin--bookmark-positions))
+            (bm-name (alist-get n positions))
+            (record (bookmark-get-bookmark bm-name 'noerror))
+            (filename (bookmark-get-filename record)))
+      (truncate-string-to-width (file-name-nondirectory filename) 12 nil nil t)
+    "<empty>"))
 
-(defvar dogears-list)
-(defvar dogears-idle)
-(defvar dogears-idle-timer)
-(defvar dogears-message)
-(defvar bookmark-save-flag)
-(defvar bookmark-use-annotations)
-(defvar bookmark-fringe-mark)
+(defun my-bookmarks--dogears-idle-status ()
+  "Return dogears idle status for transient display."
+  (if dogears-idle (format "Idle: %ss" dogears-idle) "Idle: off"))
 
 (defun my-bookmarks--bookmark-status ()
   "Return bookmark settings status for transient display."
@@ -225,55 +222,57 @@
             (run-with-idle-timer dogears-idle 'repeat #'dogears-remember)))
     (message "Dogears idle auto-remember enabled (5s)")))
 
-(defun my-bookmarks--dogears-idle-status ()
-  "Return dogears idle status for transient display."
-  (if dogears-idle
-      (format "Idle: %ss" dogears-idle)
-    "Idle: off"))
+(defun my-bookmarks--toggle-dogears-message ()
+  "Toggle dogears message on/off."
+  (interactive)
+  (setq dogears-message (not dogears-message))
+  (message "Dogears messages: %s" (if dogears-message "on" "off")))
 
-(defun my-bookmarks--javelin-status ()
-  "Return a formatted string showing current javelin slot assignments."
-  (if (not (fboundp 'javelin--bookmark-positions))
-      ""
-    (let ((positions (javelin--bookmark-positions)))
-      (if (null positions)
-          "(no pins)"
-        (mapconcat
-         (lambda (pair)
-           (let* ((pos (car pair))
-                  (bm-name (cdr pair))
-                  (record (bookmark-get-bookmark bm-name 'noerror))
-                  (display (if record
-                               (javelin--bookmark-display-target record)
-                             "?")))
-             (format "%d:%s" pos (truncate-string-to-width display 20 nil nil t))))
-         positions "  ")))))
+(defun my-bookmarks--toggle-auto-save ()
+  "Toggle bookmark auto-save on/off."
+  (interactive)
+  (setq bookmark-save-flag (not bookmark-save-flag))
+  (message "Bookmark auto-save: %s" (if bookmark-save-flag "on" "off")))
+
+(defun my-bookmarks--toggle-annotations ()
+  "Toggle bookmark annotations prompt on/off."
+  (interactive)
+  (setq bookmark-use-annotations (not bookmark-use-annotations))
+  (message "Bookmark annotations prompt: %s" (if bookmark-use-annotations "on" "off")))
+
+(defun my-bookmarks--toggle-fringe ()
+  "Toggle bookmark fringe marks on/off."
+  (interactive)
+  (setq bookmark-fringe-mark (not bookmark-fringe-mark))
+  (message "Bookmark fringe marks: %s" (if bookmark-fringe-mark "on" "off")))
+
+;;; Transient menus
+
+;;;###autoload (autoload 'my-bookmarks-settings-tmenu "my-setup-bookmarks" nil t)
+(transient-define-prefix my-bookmarks-settings-tmenu ()
+  "Bookmark settings."
+  [:description
+   my-bookmarks--dogears-idle-status
+   ("I" "Toggle idle" my-bookmarks--dogears-toggle-idle :transient t)
+   ("M" "Toggle messages" my-bookmarks--toggle-dogears-message :transient t)
+   ("D" "Toggle mode" dogears-mode :transient t)
+   ("K" "Clear history" my-bookmarks--dogears-clear :transient t)]
+
+  [:description
+   my-bookmarks--bookmark-status
+   ("s" "Toggle auto-save" my-bookmarks--toggle-auto-save :transient t)
+   ("a" "Toggle annotations" my-bookmarks--toggle-annotations :transient t)
+   ("f" "Toggle fringe" my-bookmarks--toggle-fringe :transient t)]
+
+  ["Javelin"
+   ("X" "Clear all pins" javelin-clear :transient t)]
+
+  [("q" "Back" transient-quit-one)])
 
 ;;;###autoload (autoload 'my-bookmarks-tmenu "my-setup-bookmarks" nil t)
 (transient-define-prefix my-bookmarks-tmenu ()
   "Unified bookmark management."
-  [:description
-   (lambda () (format "Javelin: %s" (my-bookmarks--javelin-status)))
-   ["Pin (Javelin)"
-    ("t" "Throw (pin here)" javelin-add-file)
-    ("T" "Assign to slot..." javelin-assign-to)
-    ("1" "Slot 1" javelin-go-to-1 :transient t)
-    ("2" "Slot 2" javelin-go-to-2 :transient t)
-    ("3" "Slot 3" javelin-go-to-3 :transient t)
-    ("4" "Slot 4" javelin-go-to-4 :transient t)
-    ("5" "Slot 5" javelin-go-to-5 :transient t)
-    ("x" "Delete slot..." javelin-delete :transient t)
-    ("X" "Clear all pins" javelin-clear :transient t)]
-
-   ["History (Dogears)"
-    ("e" "Remember here" dogears-remember :transient t)
-    ("b" "Back" dogears-back :transient t)
-    ("f" "Forward" dogears-forward :transient t)
-    ("g" "Go to..." dogears-go)
-    ("l" "List" dogears-list)
-    ("s" "Sidebar" dogears-sidebar)]
-
-   ["Emacs Bookmarks"
+  [["Bookmarks"
     ("m" "Set..." bookmark-set)
     ("j" "Jump..." consult-bookmark)
     ("J" "Jump (no preview)..." bookmark-jump)
@@ -282,6 +281,31 @@
     ("d" "Delete..." bookmark-delete)
     ("S" "Save now" bookmark-save :transient t)
     ("<" "More..." casual-editkit-bookmarks-tmenu)]
+
+   ["Javelin"
+    ("t" "Throw (pin here)" javelin-add-file)
+    ("T" "Assign to slot..." javelin-assign-to)
+    ("1" javelin-go-or-assign-to-1 :description (lambda () (my-bookmarks--javelin-slot-desc 1)) :transient t)
+    ("2" javelin-go-or-assign-to-2 :description (lambda () (my-bookmarks--javelin-slot-desc 2)) :transient t)
+    ("3" javelin-go-or-assign-to-3 :description (lambda () (my-bookmarks--javelin-slot-desc 3)) :transient t)
+    ("4" javelin-go-or-assign-to-4 :description (lambda () (my-bookmarks--javelin-slot-desc 4)) :transient t)
+    ("5" javelin-go-or-assign-to-5 :description (lambda () (my-bookmarks--javelin-slot-desc 5)) :transient t)]
+
+   ["Javelin 6-0"
+    ("6" javelin-go-or-assign-to-6 :description (lambda () (my-bookmarks--javelin-slot-desc 6)) :transient t)
+    ("7" javelin-go-or-assign-to-7 :description (lambda () (my-bookmarks--javelin-slot-desc 7)) :transient t)
+    ("8" javelin-go-or-assign-to-8 :description (lambda () (my-bookmarks--javelin-slot-desc 8)) :transient t)
+    ("9" javelin-go-or-assign-to-9 :description (lambda () (my-bookmarks--javelin-slot-desc 9)) :transient t)
+    ("0" javelin-go-or-assign-to-10 :description (lambda () (my-bookmarks--javelin-slot-desc 10)) :transient t)
+    ("x" "Delete slot..." javelin-delete :transient t)]
+
+   ["Dogears"
+    ("e" "Remember here" dogears-remember :transient t)
+    ("b" "Back" dogears-back :transient t)
+    ("f" "Forward" dogears-forward :transient t)
+    ("g" "Go to..." dogears-go)
+    ("l" "List" dogears-list)
+    ("s" "Sidebar" dogears-sidebar)]
 
    ["Registers"
     ("r" "View/jump (SPC r)" consult-register)
@@ -305,35 +329,11 @@
    ("," "Prev edit" goto-last-change-reverse :transient t)
    ("F" "Recent files" consult-recent-file)]
 
-  [:description
-   (lambda () (my-bookmarks--dogears-idle-status))
+  [""
    :class transient-row
-   ("I" "Toggle idle" my-bookmarks--dogears-toggle-idle :transient t)
-   ("M" "Toggle messages" (lambda () (interactive)
-                            (setq dogears-message (not dogears-message))
-                            (message "Dogears messages: %s" (if dogears-message "on" "off")))
-    :transient t)
-   ("D" "Toggle mode" dogears-mode :transient t)
-   ("K" "Clear history" my-bookmarks--dogears-clear :transient t)]
-
-  [:description
-   (lambda () (my-bookmarks--bookmark-status))
-   :class transient-row
-   ("C-s" "Toggle auto-save" (lambda () (interactive)
-                               (setq bookmark-save-flag (not bookmark-save-flag))
-                               (message "Bookmark auto-save: %s" (if bookmark-save-flag "on" "off")))
-    :transient t)
-   ("C-a" "Toggle annotations" (lambda () (interactive)
-                                 (setq bookmark-use-annotations (not bookmark-use-annotations))
-                                 (message "Bookmark annotations prompt: %s" (if bookmark-use-annotations "on" "off")))
-    :transient t)
-   ("C-f" "Toggle fringe" (lambda () (interactive)
-                            (setq bookmark-fringe-mark (not bookmark-fringe-mark))
-                            (message "Bookmark fringe marks: %s" (if bookmark-fringe-mark "on" "off")))
-    :transient t)
+   ("?" "Settings..." my-bookmarks-settings-tmenu)
    ("q" "Quit" transient-quit-all)])
 
-;; Global binding
 (keymap-global-set "M-g m" #'my-bookmarks-tmenu)
 
 (provide 'my-setup-bookmarks)
