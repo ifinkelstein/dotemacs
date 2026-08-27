9726e0d4fbbbba2fc86e2f426119314e55c40bb1
ilya <ilya@finkelsteinlab.org>
2026-08-27 10:39:47 -0500

    chore(custom): record package-selected-packages after 31.1 upgrade

--- custom.el
+++ custom.el
@@ -12,29 +12,32 @@
  '(package-selected-packages
    '(ace-window activities affe ag aggressive-indent aidermacs applescript-mode
                 avy-menu avy-zap bufler cape casual claude-code consult-dir
-                consult-notes consult-reftex consult-todo corfu crux ctrlf
-                deadgrep diff-hl dired-hacks dired-narrow dired-preview
-                dired-ranger dired-recent diredfl dogears dwim-shell-command
-                easy-kill-extras eat eglot-ltex-plus elfeed-goodies elfeed-org
-                elisp-def emacs-everywhere emacs-slack embark-consult embrace
-                emms exec-path-from-shell expreg fancy-dabbrev fix-word fzf
-                ghostel goggles goto-chg gptel-aibo gptel-prompts grab-mac-link
-                helpful highlight-defined highlight-quoted homebrew
-                hungry-delete imenu-list javelin jinx jq-mode json-snatcher
-                keyfreq kind-icon lambda-line lambda-themes latex-change-env
-                link-hint marginalia markdown-table-wrap mcp md-ts-mode meow
-                mixed-pitch move-text mu4e-column-faces mu4e-query mwim
+                consult-notes consult-reftex consult-todo corfu cperl-mode crux
+                csharp-mode ctrlf deadgrep dictionary diff-hl dired-hacks
+                dired-narrow dired-preview dired-ranger dired-recent diredfl
+                dogears dwim-shell-command easy-kill-extras eat editorconfig
+                eglot-ltex-plus elfeed-goodies elfeed-org elisp-def
+                elixir-ts-mode emacs-everywhere emacs-slack embark-consult
+                embrace emms exec-path-from-shell expreg faceup fancy-dabbrev
+                fix-word fzf ghostel goggles goto-chg gptel gptel-aibo
+                gptel-prompts grab-mac-link helpful highlight-defined
+                highlight-quoted homebrew hungry-delete imenu-list inheritenv
+                javelin jinx jq-mode json-snatcher keyfreq kind-icon lambda-line
+                lambda-themes latex-change-env less-css-mode link-hint lua-mode
+                marginalia markdown-table-wrap markdown-ts-mode mcp md-ts-mode
+                meow mixed-pitch move-text mu4e-column-faces mu4e-query mwim
                 nerd-icons-completion nerd-icons-corfu nerd-icons-dired nov
-                ob-applescript olivetti org-appear org-autolist
+                ob-applescript olivetti org org-appear org-autolist
                 org-bookmark-heading org-contacts org-download org-modern
                 org-mru-clock org-msg org-pomodoro org-ql org-roam
                 org-sticky-header org-timeblock org-transclusion org-web-tools
-                origami ox-pandoc pdf-tools phscroll popper popwin prism puni
-                ragmacs rainbow-delimiters rainbow-identifiers rainbow-mode
+                origami ox-pandoc pdf-tools peg phscroll popper popwin prism
+                puni ragmacs rainbow-delimiters rainbow-identifiers rainbow-mode
                 reveal-in-osx-finder revert-buffer-all rg semext slack
-                substitute svg-tag-mode tabspaces taxy-magit-section trashed
-                ultra-scroll vdiff-magit vertico visual-regexp-steroids
-                web-server whisper yasnippet-snippets))
+                substitute svg-tag-mode tabspaces taxy-magit-section timeout
+                tramp trashed ultra-scroll vdiff-magit verilog-mode vertico
+                visual-regexp-steroids wallpaper web-server which-key whisper
+                yasnippet-snippets))
  '(package-vc-selected-packages
    '((phscroll :vc-backend Git :url "https://github.com/misohena/phscroll")
      (lambda-line :url "https://codeberg.org/Lambda-Emacs/lambda-line" :branch
